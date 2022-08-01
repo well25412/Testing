6d68e285e4d0f52b1bf6d0f973d2667f46e70e68
well25412 <monishaw99@gmail.com>
2022-08-01 12:53:43 +0530

    adding test.py

---
+++ test.py
@@ -0,0 +1,128 @@
+print("hello world")
+
+for _ in range(5):
+    print("hello world")
+for i in range(5):
+    print("hello")
+
+
+
+
+names=['apple', 'google', 'gmail', 'yahoo']
+print(names)
+
+
+profile ={'fname': 'sandeep', 'lname': 'suryaprasad', 'age': 26}
+print(profile)
+
+names.append(1.5)
+names.append(10)
+names.append(profile)
+print(names)
+
+
+a = 10
+
+b = 20
+
+a + b
+30
+
+
+#add(1.3, 1.4)
+print("hello world")
+print("hi there")
+
+
+
+word = "hello world"
+
+print(word)
+
+
+word = 'hello world'
+
+print(word)
+
+word = """this is a sentence
+and this is a multiline string"""
+
+print(word)
+
+
+firstName = "sandeep"
+
+first_name = "sandeep"
+
+
+# welcome to Python's world
+word = "welcome to Python's world"
+print(word)
+word = 'welcome to Python\'s world'
+print(word)
+word = 'welcome to Python"s world'
+print(word)
+file_path = r"c:\new_release\testing\release1.2\soemthing.exe"
+
+print(file_path)
+word='welcome to Python"s world'
+
+type(word)
+word = "hello world"
+type(word)
+a = [1, 2, 3, 4]
+type(a)
+word ='hello world'
+dir(word)
+word.upper()
+u_world = word.upper()
+print(u_world)
+word = "      hello world        "
+
+s = word.strip()
+print(s)
+sentence = "hello world welcome to python"
+
+parts = sentence.split()
+print(parts)
+print(type(parts))
+type(sentence)
+sentence = "hello,world,welcome,to,python"
+
+parts = sentence.split(",")
+print(parts)
+sentence = "hello|world|welcome|to|python"
+
+sentence.split("|")
+word = "hello world"
+len(word)
+print("saddest")
+tag ="God"
+len(tag)
+tG='GOD'
+print(len(tG))
+a = 10
+print(a)
+type(a)
+dir(a)
+print('A')
+dir(word)
+word.strip()
+tag="   tiktok           "
+print(tag.strip())
+#add(a,b)
+print("Hi")
+print("Hello")
+word1="I am mad"
+print(word1)
+print(type(word1))
+word2=["jeans","shirts","jackets"]
+print(word2)
+print(type(word2))
+print(dir(word2))
+print(len(word2))
+
+
+
+
+
